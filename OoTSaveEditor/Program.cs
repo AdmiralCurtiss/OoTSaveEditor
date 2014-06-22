b690faa4774e11aed4f982f42801b0e4c72bb8f5
Admiral H. Curtiss <pikachu025@gmail.com>
2014-06-22 16:48:09 +0200

    Moving the OoT Save Editor out of HyoutaTools.

---
+++ OoTSaveEditor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HyoutaTools.Other.N64.OoTSaveEditor {
+	class Program {
+		public static int Main( string[] args ) {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run( new OoTSaveEditForm() );
+			return 0;
+		}
+	}
+}
